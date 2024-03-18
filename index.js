89baeff04e6b8467efe6e083b9b56b8e5d07459e
silas <silasgebhart12@gmail.com>
2024-03-18 14:56:13 -0500

    final commit

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Add your code here
+function submitData(name, email) {
+  const formData = {
+    name: name,
+    email: email,
+  };
+
+  const configObj = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify(formData),
+  };
+
+  return fetch("http://localhost:3000/users", configObj)
+    .then((response) => response.json())
+    .then((data) => {
+      const id = data.id;
+      document.body.innerHTML += id; // Append id to the DOM
+    })
+    .catch((error) => {
+      console.error("Error:", error.message);
+      document.body.innerHTML += error.message; // Append error message to the DOM
+    });
+}
